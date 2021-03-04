5df7dcdb6a2d8309b51596a40e1b834033b94769
Maria Morais <61720083+MariargMorais@users.noreply.github.com>
2021-03-03 22:51:34 -0300

    start

---
+++ Vivere/src/main/java/com/MariaRGMorais/Vivere/VivereApplication.java
@@ -0,0 +1,13 @@
+package com.MariaRGMorais.Vivere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivereApplication.class, args);
+	}
+
+}
---
+++ Vivere/src/test/java/com/MariaRGMorais/Vivere/VivereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MariaRGMorais.Vivere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
